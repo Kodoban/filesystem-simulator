5a253a157296c5a7e799efb9ad3a37cc67ab497e
Kodoban <gitaliasub.meshf@simplelogin.fr>
2024-06-07 16:17:40 +0300

    Create HomeDirectory CRUD endpoints

--- src/main/java/dev/filesystemsim/demo/features/homeDirectory/HomeDirectoryRepository.java
+++ src/main/java/dev/filesystemsim/demo/features/homeDirectory/HomeDirectoryRepository.java
@@ -1,10 +1,11 @@
 package dev.filesystemsim.demo.features.homeDirectory;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import dev.filesystemsim.demo.features.directory.DirectoryRepository;
+import dev.filesystemsim.demo.features.homeDirectory.definition.HomeDirectoryEntity;
 
 @Repository
-public interface HomeDirectoryRepository extends DirectoryRepository {
+public interface HomeDirectoryRepository extends CrudRepository<HomeDirectoryEntity, Integer> {
 
 }
---
+++ src/main/java/dev/filesystemsim/demo/features/homeDirectory/service/HomeDirectoryServiceImpl.java
@@ -0,0 +1,53 @@
+package dev.filesystemsim.demo.features.homeDirectory.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import dev.filesystemsim.demo.features.homeDirectory.HomeDirectoryRepository;
+import dev.filesystemsim.demo.features.homeDirectory.definition.HomeDirectoryDto;
+import dev.filesystemsim.demo.features.homeDirectory.definition.HomeDirectoryEntity;
+import dev.filesystemsim.demo.features.homeDirectory.mapper.HomeDirectoryMapper;
+
+@Service
+public class HomeDirectoryServiceImpl implements HomeDirectoryService {
+
+    private final HomeDirectoryMapper homeDirectoryMapper;
+    private final HomeDirectoryRepository homeDirectoryRepository;
+    
+    public HomeDirectoryServiceImpl(HomeDirectoryMapper homeDirectoryMapper, HomeDirectoryRepository homeDirectoryRepository) {
+        this.homeDirectoryMapper = homeDirectoryMapper;
+        this.homeDirectoryRepository = homeDirectoryRepository;
+    }
+
+    @Override
+    public HomeDirectoryDto save(HomeDirectoryDto homeDirectoryDto) {
+        HomeDirectoryEntity homeDirectoryEntity = homeDirectoryMapper.mapDtoToEntity(homeDirectoryDto);
+        HomeDirectoryEntity savedHomeDirectoryEntity = homeDirectoryRepository.save(homeDirectoryEntity);
+        return homeDirectoryMapper.mapEntityToDto(savedHomeDirectoryEntity);
+    }
+
+    @Override
+    public Optional<HomeDirectoryDto> getHomeDirectoryById(Integer id) {
+        return homeDirectoryRepository.findById(id).map(
+            homeDirectoryMapper::mapEntityToDto
+        );
+    }
+
+    @Override
+    public HomeDirectoryDto update(Integer id, HomeDirectoryDto directoryDto) {
+        HomeDirectoryEntity homeDirectoryEntity = homeDirectoryMapper.mapDtoToEntity(directoryDto);
+        HomeDirectoryEntity savedHomeDirectoryEntity = homeDirectoryRepository.findById(id).map(existingHomeDirectory -> {
+                Optional.ofNullable(homeDirectoryEntity.getName()).ifPresent(existingHomeDirectory::setName);
+                Optional.ofNullable(homeDirectoryEntity.getContent()).ifPresent(existingHomeDirectory::setContent);
+                return homeDirectoryRepository.save(existingHomeDirectory);
+        }).orElseThrow(() -> new RuntimeException("User does not exist"));
+        return homeDirectoryMapper.mapEntityToDto(savedHomeDirectoryEntity);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        homeDirectoryRepository.deleteById(id);
+    }
+
+}
